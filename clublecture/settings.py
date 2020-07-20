d18011b09002acf355d8c3e3eaa88b539b63b805
Nicolas Lattemann <56591727+Nydragon00@users.noreply.github.com>
2020-07-20 16:35:10 +0200

    creaated post table in database

--- clublecture/settings.py
+++ clublecture/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'post_back',
+    'post_back.apps.PostBackConfig',
 ]
 
 MIDDLEWARE = [
--- post_back/models.py
+++ post_back/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    user_id = models.IntegerField()
+    date = models.DateField()
+    description = models.CharField(max_length=500)
