956054b067936210d8f6f1ffbb1f19b69a07ae3f
Jane Lane <loan.does@gmail.com>
2020-07-21 14:55:34 +0200

    ajout urls media post

--- post_back/urls.py
+++ post_back/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-
+from django.urls import include, path
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'posts', views.PostViewSet)
+router.register(r'medias', views.MediaViewSet)
+
 urlpatterns = [
+	path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- post_back/views.py
+++ post_back/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# from django.shortcuts import render
+# from django.http import HttpResponse
 from rest_framework import viewsets
 from .serializers import PostSerializer, MediaSerializer
 from .models import Post, Media
 
 # Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+	queryset = Post.objects.all().order_by('description')
+	serializers_class = PostSerializer
+
+class MediaViewSet(viewsets.ModelViewSet):
+	queryset = Media.objects.all().order_by('post_id')
+	serializers_class = MediaSerializer
