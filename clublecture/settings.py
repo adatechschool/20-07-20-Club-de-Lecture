ec991cd7e230b3181719ea58d863b2895ce3d235
Nicolas Lattemann <56591727+nLatt@users.noreply.github.com>
2020-07-24 09:00:37 +0200

    Update settings.py

--- clublecture/settings.py
+++ clublecture/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
