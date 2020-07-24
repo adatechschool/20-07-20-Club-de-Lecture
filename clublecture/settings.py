c0c0beb24498304feff3a31765ff71df06db45f6
Nicolas Lattemann <56591727+Nydragon00@users.noreply.github.com>
2020-07-24 14:55:57 +0200

    type : to =

--- clublecture/settings.py
+++ clublecture/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS: ['localhost', "clublecture.herokuapp.com"]
+ALLOWED_HOSTS = ['localhost', "clublecture.herokuapp.com"]
 
 
 # Password validation
