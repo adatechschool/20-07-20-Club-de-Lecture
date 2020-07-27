66550252a4e3501cee9b9d450fcb7f66cafba61d
Nicolas Lattemann <56591727+Nydragon00@users.noreply.github.com>
2020-07-27 16:28:43 +0200

    avatar is optional

--- api/models.py
+++ api/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     user_name = models.CharField(max_length=20, blank=False, unique=True)
     email = models.CharField(max_length=30, blank=False,  unique=True)
     password = models.CharField(max_length=10000, blank=False)
-    avatar = models.CharField(max_length=200)
+    avatar = models.CharField(max_length=200, blank=True)
 
 # generate the table to store posts
 class Post(models.Model):
