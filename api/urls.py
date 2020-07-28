f975a38f0ab3aa335dab0cf352484178a571285d
Nicolas Lattemann <56591727+Nydragon00@users.noreply.github.com>
2020-07-28 11:04:56 +0200

    Under the hood url changes

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r"^api/users$", views.users),
-	url(r"^api/posts$", views.posts),
-	url(r"^api/posts/(?P<pk>[0-9]+)$", views.post),
+	url(r"^users$", views.users),
+	url(r"^posts$", views.posts),
+	url(r"^posts/(?P<pk>[0-9]+)$", views.post),
 	# url(r"^api/medias$", views.Medias),
 ]
