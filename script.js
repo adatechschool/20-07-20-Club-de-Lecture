180023f52ee6f5c18b936335ace34ce02909c216
del <w.bounkeua@gmail.com>
2020-07-31 14:57:27 +0200

    recupération de la réponse du serveur pour création de compte

--- script.js
+++ script.js
@@ -9,9 +9,14 @@ function envoiDonnees() {
   let json = JSON.stringify(object);
 
   var req = new XMLHttpRequest();
+  req.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 201){
+      document.getElementById("test").innerHTML = "votre compte a bien été créé";
+    }
+  };
   req.open("POST", "https://clublecture.herokuapp.com/api/users");
-
   req.send(json);
-  console.log(json);
+
   return
 }
+
