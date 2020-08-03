0f764a485f2c0e82bde5590467a20088aa68cc60
Nicolas Lattemann <6582485n@gmail.com>
2020-08-03 18:01:59 +0200

    avatar Charfield to URLField

--- api/models.py
+++ api/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     user_name = models.CharField(max_length=20, blank=False, unique=True)
     email = models.EmailField(max_length=40, blank=False,  unique=True)
     password = models.CharField(max_length=10000, blank=False)
-    avatar = models.CharField(max_length=200, blank=True)
+    avatar = models.URLField(max_length=200, blank=True)
 
     def save(self, *args, **kwargs):
         super().save()
