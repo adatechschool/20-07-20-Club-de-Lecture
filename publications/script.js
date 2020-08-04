cdf2dbe8b70fb39c3cc21e1e481a01c653b9ecbe
jessyjones <alya@moov.mg>
2020-08-04 17:23:05 +0200

    Mise en place des pre-requis pour l'édition des publications
    Ajout d'un champ texte (caché) pour chaque publication afin de permettre sa modification

--- publications/script.js
+++ publications/script.js
@@ -7,7 +7,7 @@ function fetch_publications() {
   req.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var publications = JSON.parse(this.responseText)
-      for(i in publications) {
+      for (i in publications) {
         document.body.append(create_div(publications[i]));
       }
 
@@ -19,16 +19,26 @@ function fetch_publications() {
 
 function create_div(publication) {
   var new_div = document.createElement('div')
-  new_div.innerHTML = publication.user +
-                      ' ' +
-                      publication.description +
-                      ' ' +
-                      publication.creation_date;
+  new_div.innerHTML = '<div class = "userName"> ' + publication.user_name +
+    '</div> <div class = "publicationContent"> ' +
+    publication.description +
+    '</div>' +
+    '<textarea name="" id="boiteDeSaisie_'
+    + publication.id
+    + '" style="display:none;"> ' +
+    publication.description +
+    '"</textarea>' +
+    ' <div class = "publicationDate"> ' +
+    publication.creation_date + '</div>' +
+    '<button type="button" onclick="modifierPublication('
+    + publication.id
+    +')">Modifier' +
+    '</button>';
   return new_div;
 }
 
-      // <div id="cunediv">
-      //   <p class="user"></p>
-      //   <p class="description"></p>
-      //   <p class="creation_date"></p>
-      // </div>
+function modifierPublication(numeroClique) {
+  console.log("fonction de modification de la publication numero " + numeroClique)
+  let boite = document.getElementById("boiteDeSaisie_"+numeroClique)
+  boite.style.display = "block";
+}
